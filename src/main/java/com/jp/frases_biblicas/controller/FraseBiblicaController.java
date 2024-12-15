aeb24fd29af32bf3cd2c0812a89521b864954479
joaopedro00017 <joaopedrodiasandrade8@gmail.com>
2024-12-15 15:59:57 -0300

    Atualização 1.2 do Projeto(não está encontrando nada no banco de dados)

--- src/main/java/com/jp/frases_biblicas/controller/FraseBiblicaController.java
+++ src/main/java/com/jp/frases_biblicas/controller/FraseBiblicaController.java
@@ -3,6 +3,8 @@ package com.jp.frases_biblicas.controller;
 import com.jp.frases_biblicas.frasesBiblicas.FraseBiblica;
 import com.jp.frases_biblicas.frasesBiblicasRepository.FraseBiblicaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,8 +14,12 @@ public class FraseBiblicaController {
     private FraseBiblicaRepository fraseBiblicaRepository;
 
     @GetMapping("/frase-aleatoria")
-    public FraseBiblica getFraseAleatoria() {
-        return fraseBiblicaRepository.findRandomFrase();
+    public String getFraseAleatoria() {
+        FraseBiblica frase = fraseBiblicaRepository.findRandomFrase();
+        if (frase != null) {
+            return frase.getFrase();
+        } else {
+            return "Não foi possível encontrar nenhuma frase no momento.";
+        }
     }
 }
-
--- src/main/java/com/jp/frases_biblicas/frasesBiblicas/FraseBiblica.java
+++ src/main/java/com/jp/frases_biblicas/frasesBiblicas/FraseBiblica.java
@@ -1,7 +1,6 @@
 package com.jp.frases_biblicas.frasesBiblicas;
 
 import jakarta.persistence.*;
-
 @Entity
 public class FraseBiblica {
     @Id
@@ -11,5 +10,20 @@ public class FraseBiblica {
     @Column(columnDefinition = "TEXT")
     private String frase;
 
-    // Getters and setters
+    // Getter and setter methods
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFrase() {
+        return frase;
+    }
+
+    public void setFrase(String frase) {
+        this.frase = frase;
+    }
 }
